94835e69cc4f92dc5eab02d999d76ffa11ead4a8
Nélson Guillermo <nelsonguillermo.dev@gmail.com>
2025-03-12 10:24:03 -0600

    Agregar propiedades al modelo pedido.js

--- server/models/Pedido.js
+++ server/models/Pedido.js
@@ -6,12 +6,6 @@ const pedidoSchema = new mongoose.Schema({
     tipoComida: { type: String, required: true },
     pagado: { type: Boolean, default: false },
     entregado: { type: Boolean, default: false },
-    dineroRecibido: { type: Number, required: false },  
-    precioComida:  {type: Number, required: false }, 
-    cambio: { 
-        type: Number, 
-        default: function() { return this.dineroRecibido - this.precioComida; } 
-    },
     cambioEntregado: {type: Boolean, default: false}
 });
 
